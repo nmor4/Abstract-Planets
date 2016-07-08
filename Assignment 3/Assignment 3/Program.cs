7f5be4310c21a65b927d8ce6a2a73d53d7bb507b
nmor4 <nick-morin@hotmail.com>
2016-07-08 11:33:32 -0400

    Initial commit

---
+++ Assignment 3/Assignment 3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/* 
+ * Author: Nick Morin - 300654901
+ * Program: COMP123 Assignment 3 - Abstract Planets
+ * Date Last Modified: 2016-07-08
+ */
+
+
+namespace Assignment_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
